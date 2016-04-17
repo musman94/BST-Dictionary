275729fdc440de120283f6ad65573c2949ded807
musman94 <usmanfcbarca@icloud.com>
2016-04-18 01:03:57 +0300

    Initial Commit

---
+++ HW4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW4
+//
+//  Created by Muhammad Usman on 4/18/16.
+//  Copyright © 2016 Muhammad Usman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
